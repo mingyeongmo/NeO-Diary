18e67071169e5289bc0dae6a5317d2b2f821cf03
mingyeongmo <mingyeongmp@naver.com>
2024-04-09 23:25:04 +0900

    로그아웃시 navigate 처리

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { auth } from "../../firebase";
 import styles from "./style.module.scss";
 
 const Home = () => {
-  const logOut = () => {
-    auth.signOut();
+  const navigate = useNavigate();
+
+  const LogOut = async () => {
+    const ok = window.confirm("로그아웃 하시겠습니까?");
+    if (ok) {
+      await auth.signOut();
+      navigate("/login");
+    }
   };
 
   return (
     <div>
-      <button onClick={logOut}>Log Out</button>
+      <button onClick={LogOut}>Log Out</button>
       <h1>나의 일기</h1>
       <Link to="/diary">일기 쓰러가기</Link>
     </div>
