ea7dffaf74aeea6d130b50939a9e4721d6b5dc77
mingyeongmo <mingyeongmp@naver.com>
2024-04-11 23:37:57 +0900

    간단하게 일기 생성 하는 파일 생성

--- src/pages/Diary/index.tsx
+++ src/pages/Diary/index.tsx
@@ -1,10 +1,36 @@
+import React, { useState } from "react";
+import * as S from "./style";
+
 const Diary = () => {
+  const [isLoading, setLoading] = useState(false);
+  const [diary, setDiary] = useState("");
+  const [file, setFile] = useState<File | null>(null);
+
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setDiary(e.target.value);
+  };
+
+  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    if (files && files.length === 1) {
+      setFile(files[0]);
+    }
+  };
   return (
-    <>
+    <S.Form>
       <div>제목 & 날씨</div>
-      <div>그림 그리는곳</div>
-      <div>글 쓰는 곳</div>
-    </>
+      <S.AttachFileBtn htmlFor="file">
+        {file ? "사진 넣음" : "사진 넣기"}
+      </S.AttachFileBtn>
+      <S.AttachFileInput
+        onChange={onFileChange}
+        type="file"
+        id="file"
+        accept="image/*"
+      ></S.AttachFileInput>
+      <S.TextArea value={diary} />
+      <S.Button type="submit">{isLoading ? "생성중" : "일기 생성"}</S.Button>
+    </S.Form>
   );
 };
 
---
+++ src/pages/Diary/style.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const AttachFileBtn = styled.label`
+  padding: 10px 0px;
+`;
+
+export const AttachFileInput = styled.input`
+  display: none;
+`;
+
+export const TextArea = styled.textarea`
+  border: 2px solid;
+  padding: 20px;
+  border-radius: 20px;
+  font-size: 16px;
+  width: 100%;
+  resize: none;
+`;
+
+export const Button = styled.button``;
