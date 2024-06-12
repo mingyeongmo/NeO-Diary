69811b3999e02eec25e34f3f3f247d5c2cd6d79c
mingyeongmo <mingyeongmp@naver.com>
2024-06-12 22:21:34 +0900

    날짜 파일 추가

---
+++ src/pages/Diary/Calendar.tsx
@@ -0,0 +1,14 @@
+const Calendar = () => {
+  let today = new Date();
+
+  let year = today.getFullYear(); // 년도
+  let month = today.getMonth() + 1; // 월
+  let date = today.getDate(); // 날짜
+  return (
+    <>
+      <h1>{`${year} / ${month} / ${date}`}</h1>
+    </>
+  );
+};
+
+export default Calendar;
