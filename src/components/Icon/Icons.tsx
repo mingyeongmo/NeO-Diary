f40bb10e0b24e1c21927dd23e9b70cd2ff2371aa
mingyeongmo <mingyeongmp@naver.com>
2024-06-27 22:13:58 +0900

    달력 추가 기능

--- src/components/Icon/Icons.tsx
+++ src/components/Icon/Icons.tsx
@@ -71,3 +71,35 @@ export const Delete = () => (
     <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
   </svg>
 );
+
+export const LeftBtn = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    fill="currentColor"
+    className="bi bi-chevron-left"
+    viewBox="0 0 16 16"
+  >
+    <path
+      fill-rule="evenodd"
+      d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"
+    />
+  </svg>
+);
+
+export const RightBtn = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    fill="currentColor"
+    className="bi bi-chevron-right"
+    viewBox="0 0 16 16"
+  >
+    <path
+      fill-rule="evenodd"
+      d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"
+    />
+  </svg>
+);
--- src/pages/Diary/Calendar/Calendar.tsx
+++ src/pages/Diary/Calendar/Calendar.tsx
@@ -1,15 +1,83 @@
 import { SetStateAction, useState } from "react";
 import DatePicker from "react-datepicker";
+import { getMonth, getYear } from "date-fns";
 import "react-datepicker/dist/react-datepicker.css";
+import { ko } from "date-fns/locale";
+import { LeftBtn, RightBtn } from "components/Icon/Icons";
 import styles from "./Calendar.module.scss";
 
 const Calendar = () => {
   const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
 
+  const YEARS = Array.from(
+    { length: getYear(new Date()) + 1 - 2000 },
+    (_, i) => getYear(new Date()) - i
+  );
+  const MONTHS = [
+    "1월",
+    "2월",
+    "3월",
+    "4월",
+    "5월",
+    "6월",
+    "7월",
+    "8월",
+    "9월",
+    "10월",
+    "11월",
+    "12월",
+  ];
+
   return (
     <div className={styles.calenderContainer}>
       <DatePicker
         className={styles.datePicker}
+        locale={ko}
+        renderCustomHeader={({
+          date,
+          changeYear,
+          decreaseMonth,
+          increaseMonth,
+          prevMonthButtonDisabled,
+          nextMonthButtonDisabled,
+        }) => (
+          <div className={styles.customHeaderContainer}>
+            <div>
+              <button
+                type="button"
+                onClick={decreaseMonth}
+                className={styles.monthBtn}
+                disabled={prevMonthButtonDisabled}
+              >
+                <LeftBtn />
+              </button>
+            </div>
+            <div>
+              <select
+                value={getYear(date)}
+                className={styles.year}
+                onChange={({ target: { value } }) => changeYear(+value)}
+              >
+                {YEARS.map((option) => (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+              <span className={styles.month}>{MONTHS[getMonth(date)]}</span>
+            </div>
+            <div>
+              <button
+                type="button"
+                onClick={increaseMonth}
+                className={styles.monthBtn}
+                disabled={nextMonthButtonDisabled}
+              >
+                <RightBtn />
+              </button>
+            </div>
+          </div>
+        )}
         dateFormat="yyyy.MM.dd" // 날짜 형태
         shouldCloseOnSelect // 날짜를 선택하면 datepicker가 자동으로 닫힘
         minDate={new Date("2000-01-01")} // minDate 이전 날짜 선택 불가
